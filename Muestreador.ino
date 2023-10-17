d57aa5aa1cf9f651afb2da5acf166908ff2b6d75
2017153854 <80356865+2017153854@users.noreply.github.com>
2023-10-16 19:11:35 -0600

    Update Muestreador.ino
    Código de arduino completo

--- Muestreador.ino
+++ Muestreador.ino
@@ -6,6 +6,7 @@ byte escala = 0;
 byte escala2 = 0;
 byte ch1 = 0;
 byte ch2 = 0;
+int canal1_full = 0;  //Auxiliar para canales simultáneos 
 
 
 
@@ -24,17 +25,24 @@ void setup() {
     ADCSRA |= (1 << ADATE); // Free-running
     ADCSRA |= (1 << ADIE); // habilitar interrupciones del ADC
   
-    ADCSRA &= ~(1 << ADPS2) & ~(1 << ADPS1) & ~(1 << ADPS0);    //Prescaler en 2
+    ADCSRA &= ~(1 << ADPS2) & ~(1 << ADPS1) & ~(1 << ADPS0);    //Prescaler en 128
   
     sei(); // Habilita las interrupciones globales
   
-    // Pines para las escalas
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
-    pinMode(7, OUTPUT);
+    // Pines para las escalas del canal 1
+    pinMode(2, OUTPUT); //10mV
+    pinMode(3, OUTPUT); //100mV
+    pinMode(4, OUTPUT); //1V
+    pinMode(5, OUTPUT); //2.5V
+    pinMode(6, OUTPUT); //5V
+    pinMode(7, OUTPUT); //10V
+    // Pines para las escalas del canal 1
+    pinMode(8, OUTPUT); //10mV
+    pinMode(9, OUTPUT); //100mV
+    pinMode(10, OUTPUT); //1V
+    pinMode(11, OUTPUT); //2.5V
+    pinMode(12, OUTPUT); //5V
+    pinMode(13, OUTPUT); //10V
 }
 
 
@@ -50,22 +58,30 @@ void loop() {
     //Primero revisar si la compu ha enviado algo
     if (Serial.available() > 0) {
       byte comando = Serial.read();  // Recibir la instrucción
-      ch1 = comando & 0b00000001;  // bit para el estado del canal 1
-      ch2 = (comando>>1) & 0b00000001;  // bit para el estado del canal 2
-      escala = (comando >> 2) & 0b00000111;   // Bits 2, 3 y 4 para la escala
-      escala2 = (comando >> 5) & 0b00000111;  // Bits 5, 6 y 7 para la escala 2
-      if (ch1==0 && ch2==0) {
-        cant_canales = 0; // apagar los dos canales
-      } else if ((ch1==1 && ch2==0) || (ch1==0 && ch2==1)) {
-        cant_canales = 1;
-      } else if (ch1==1 && ch2==1) {
-        cant_canales = 2;
+      if (comando == 0b11111110){
+        canal1_full = 0;
+      }
+      else if (comando == 0b11111111) {
+        canal1_full = 1;
+      }
+      else {
+        ch1 = comando & 0b00000001;  // bit para el estado del canal 1
+        ch2 = (comando>>1) & 0b00000001;  // bit para el estado del canal 2
+        escala = (comando >> 2) & 0b00000111;   // Bits 2, 3 y 4 para la escala
+        escala2 = (comando >> 5) & 0b00000111;  // Bits 5, 6 y 7 para la escala 2
+        if (ch1==0 && ch2==0) {
+          cant_canales = 0; // apagar los dos canales
+        } else if ((ch1==1 && ch2==0) || (ch1==0 && ch2==1)) {
+          cant_canales = 1;
+        } else if (ch1==1 && ch2==1) {
+          cant_canales = 2;
+        }
       }
     }
 
 
     //-------------------------------------------------------------------------
-    //Editar escala del DAS
+    //Editar escala del canal 1
     switch (escala) {
       case 0: //Escala 0: 10 mV
         digitalWrite(6, LOW);
@@ -130,6 +146,72 @@ void loop() {
 
 
 
+    //-------------------------------------------------------------------------
+    //Editar escala del canal 2
+    switch (escala2) {
+      case 0: //Escala 0: 10 mV
+        digitalWrite(8, HIGH);
+        digitalWrite(9, LOW);
+        digitalWrite(10, LOW);
+        digitalWrite(11, LOW);
+        digitalWrite(12, LOW);
+        digitalWrite(13, LOW);
+        break;
+      case 1: //Escala 1: 100mV
+        digitalWrite(8, LOW);
+        digitalWrite(9, HIGH);
+        digitalWrite(10, LOW);
+        digitalWrite(11, LOW);
+        digitalWrite(12, LOW);
+        digitalWrite(13, LOW);
+        break;
+      case 2: //Escala 2: 1V
+        digitalWrite(8, LOW);
+        digitalWrite(9, LOW);
+        digitalWrite(10, HIGH);
+        digitalWrite(11, LOW);
+        digitalWrite(12, LOW);
+        digitalWrite(13, LOW);
+        break;
+      case 3: //Escala 3: 2.5V
+        digitalWrite(8, LOW);
+        digitalWrite(9, LOW);
+        digitalWrite(10, LOW);
+        digitalWrite(11, HIGH);
+        digitalWrite(12, LOW);
+        digitalWrite(13, LOW);
+        break;
+      case 4: //Escala 4: 5V
+        digitalWrite(8, LOW);
+        digitalWrite(9, LOW);
+        digitalWrite(10, LOW);
+        digitalWrite(11, LOW);
+        digitalWrite(12, HIGH);
+        digitalWrite(13, LOW);
+        break;
+      case 5: //Escala 5: 10V
+        digitalWrite(8, LOW);
+        digitalWrite(9, LOW);
+        digitalWrite(10, LOW);
+        digitalWrite(11, LOW);
+        digitalWrite(12, LOW);
+        digitalWrite(13, HIGH);
+        break;
+      /*
+      default:
+        digitalWrite(2, HIGH);
+        digitalWrite(3, LOW);
+        digitalWrite(4, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(7, LOW);
+        break;
+      */
+    }
+
+
+
+
 
     //-------------------------------------------------------------------------
     //Mayor jerarquía: canales apagos
@@ -143,7 +225,7 @@ void loop() {
     else {
         ADCSRA |= (1 << ADEN);  //Enciendo el adc 
         
-        if (cant_canales == 1){   //Segunda jerarquía: la velocidad
+        if (cant_canales == 1){   //Acción para alimentar un canal
           if (ch1 == true){ //Para el canal 1
             ADMUX &= ~(1 << MUX0); //Escojo el canal 1 (ADC0) (MUX[3:0]=0000)
             ADCSRA |= (1 << ADSC);  //Arranco la conversión
@@ -154,25 +236,23 @@ void loop() {
           }
         }
   
-        /*
-        else {    //Acción para alimentar los dos canales
-          //ADCSRA |= (1 << ADPS2) | (1 << ADPS1); // Prescaler en 64
-          //ADCSRA &= ~(1 << ADPS0);
-          if (ch1 == true){ //Para el canal 1
-            ADMUX &= ~(0b00000001);  //Canal 0 del MUX del ADC
-            ADCSRA |= (0b01000000);  //Arracar conversiones
-            delay(1);
-          }if (ch2 == true){  //Para el canal 2
-            ADMUX |= (0b00000001);  //Canal 1 del MUX del ADC
-            ADCSRA |= (0b01000000);  //Arracar conversiones
+        
+        if (cant_canales == 2) {    //Acción para alimentar los dos canales
+          if (ch1 == true && canal1_full == 0){ //Para el canal 1
+            ADMUX &= ~(1 << MUX0); //Escojo el canal 1 (ADC0) (MUX[3:0]=0000)
+            ADCSRA |= (1 << ADSC);  //Arranco la conversión
+          }
+          if (ch2 == true && canal1_full == 1) {  //Para el canal 2
+            ADMUX |= (1 << MUX0); //Escojo el canal 2 (ADC1) (MUX[3:0]=0001)
+            ADCSRA |= (1 << ADSC);  //Arranco la conversión
           }
         }
-        */
+        
 
         
     }
 
-  //delay(1);
+
 }
 
 
@@ -189,7 +269,6 @@ ISR(ADC_vect) {
   int dato = ADCH;
   Serial.println(dato);
 }
-
 
 
 
